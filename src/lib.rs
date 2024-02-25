d6fd5d21b19490e28540e335037ec5d981185439
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2024-02-25 14:20:58 +0200

    Add support for SDMA ucode
    Signed-off-by: Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,66 +4,89 @@
 use binaryninja::{
     binaryview::{BinaryView, BinaryViewBase, BinaryViewExt},
     command::{register_for_address, AddressCommand},
+    symbol::Symbol,
     Endianness,
 };
 
-struct ExtractorCommand;
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum FirmwareType {
+    Gc,
+    Sdma,
+}
+
+impl FirmwareType {
+    fn size_field_off(self) -> u64 {
+        match self {
+            Self::Gc => 0xC,
+            Self::Sdma => 0x8,
+        }
+    }
+
+    fn off_field_off(self) -> u64 {
+        match self {
+            Self::Gc => 0x20,
+            Self::Sdma => 0x10,
+        }
+    }
+}
+
+struct ExtractorCommand(FirmwareType);
 
 impl ExtractorCommand {
-    fn read_fw_size(view: &BinaryView, offset: u64) -> Option<u32> {
-        let data = view.read_vec(offset + 0xC, 4);
+    fn new(ty: FirmwareType) -> Self {
+        Self(ty)
+    }
+
+    fn read_fw_size(&self, view: &BinaryView, offset: u64) -> Option<u32> {
+        let data = view.read_vec(offset + self.0.size_field_off(), 4);
         Some(match view.default_endianness() {
             Endianness::LittleEndian => u32::from_le_bytes(data.as_slice().try_into().ok()?),
             Endianness::BigEndian => u32::from_be_bytes(data.as_slice().try_into().ok()?),
         })
     }
 
-    fn read_fw_off(view: &BinaryView, offset: u64) -> Option<u64> {
-        let data = view.read_vec(offset + 0x20, view.address_size());
+    fn read_fw_off(&self, view: &BinaryView, offset: u64) -> Option<u64> {
+        let data = view.read_vec(offset + self.0.off_field_off(), view.address_size());
         Some(match view.default_endianness() {
             Endianness::LittleEndian => u64::from_le_bytes(data.as_slice().try_into().ok()?),
             Endianness::BigEndian => u64::from_be_bytes(data.as_slice().try_into().ok()?),
         })
     }
 
-    fn read_fw_info(view: &BinaryView, offset: u64) -> Option<(u64, u32)> {
-        Self::read_fw_off(view, offset)
-            .and_then(|fw_off| Self::read_fw_size(view, offset).map(|size| (fw_off, size)))
+    fn read_fw_info(&self, view: &BinaryView, offset: u64) -> Option<(u64, u32)> {
+        self.read_fw_off(view, offset)
+            .and_then(|fw_off| self.read_fw_size(view, offset).map(|size| (fw_off, size)))
     }
 
-    fn read_fw_info_of_sym(view: &BinaryView, offset: u64) -> Option<(String, u64, u32)> {
-        view.symbol_by_address(offset).ok().and_then(|v| {
-            Self::read_fw_info(view, v.address()).map(|(fw_off, fw_size)| {
-                (
-                    v.full_name()
-                        .as_str()
-                        .strip_prefix('_')
-                        .map(String::from)
-                        .unwrap_or_else(|| v.full_name().as_str().to_owned()),
-                    fw_off,
-                    fw_size,
-                )
-            })
-        })
+    fn sym_to_fw_name(sym: &Symbol) -> String {
+        let full_name = sym.full_name();
+        full_name
+            .as_str()
+            .strip_prefix('_')
+            .unwrap_or_else(|| full_name.as_str())
+            .to_owned()
+    }
+
+    fn read_fw_info_of_sym(&self, view: &BinaryView, offset: u64) -> Option<(String, u64, u32)> {
+        let (fw_name, address) = view
+            .symbol_by_address(offset)
+            .map(|v| (Self::sym_to_fw_name(&v), v.address()))
+            .unwrap_or_else(|_| (format!("data_{offset:X}"), offset));
+        self.read_fw_info(view, address)
+            .map(|(fw_off, fw_size)| (fw_name, fw_off, fw_size))
     }
 }
 
 impl AddressCommand for ExtractorCommand {
     fn valid(&self, view: &BinaryView, addr: u64) -> bool {
-        let Some((_, fw_off, fw_size)) = Self::read_fw_info_of_sym(view, addr) else {
+        let Some((_, fw_off, fw_size)) = self.read_fw_info_of_sym(view, addr) else {
             return false;
         };
         view.offset_valid(fw_off) && view.offset_valid(fw_off + u64::from(fw_size))
     }
 
     fn action(&self, view: &BinaryView, addr: u64) {
-        let Some((name, fw_off, fw_size)) = Self::read_fw_info_of_sym(view, addr) else {
-            rfd::MessageDialog::new()
-                .set_level(rfd::MessageLevel::Error)
-                .set_title("Well")
-                .set_description("There's no symbol here, bozo")
-                .set_buttons(rfd::MessageButtons::OkCustom("Oh".into()))
-                .show();
+        let Some((name, fw_off, fw_size)) = self.read_fw_info_of_sym(view, addr) else {
             return;
         };
         let data = view.read_vec(fw_off, fw_size.try_into().unwrap());
@@ -72,41 +95,31 @@ impl AddressCommand for ExtractorCommand {
             .set_title(format!("Save {name}"))
             .save_file()
         else {
-            rfd::MessageDialog::new()
-                .set_level(rfd::MessageLevel::Info)
-                .set_title("Well")
-                .set_description("Ok then")
-                .set_buttons(rfd::MessageButtons::OkCustom("Yeah".into()))
-                .show();
             return;
         };
-        match std::fs::write(path, data) {
-            Ok(_) => {
-                rfd::MessageDialog::new()
-                    .set_level(rfd::MessageLevel::Info)
-                    .set_title("Nice")
-                    .set_description("File was saved")
-                    .set_buttons(rfd::MessageButtons::OkCustom("Nice".into()))
-                    .show();
-            }
-            Err(e) => {
-                rfd::MessageDialog::new()
-                    .set_level(rfd::MessageLevel::Info)
-                    .set_title("Whoops")
-                    .set_description(format!("File was not saved: {e}"))
-                    .set_buttons(rfd::MessageButtons::OkCustom("Well, shit".into()))
-                    .show();
-            }
-        }
+        let Err(e) = std::fs::write(path, data) else {
+            return;
+        };
+        rfd::MessageDialog::new()
+            .set_level(rfd::MessageLevel::Info)
+            .set_title("Whoops")
+            .set_description(format!("File was not saved: {e}"))
+            .set_buttons(rfd::MessageButtons::OkCustom("Well, shit".into()))
+            .show();
     }
 }
 
 #[no_mangle]
 pub extern "C" fn CorePluginInit() -> bool {
     register_for_address(
-        "ChefKiss\\Extract AMD Catalyst Firmware",
+        "ChefKiss\\Extract GC firmware",
         "",
-        ExtractorCommand,
+        ExtractorCommand::new(FirmwareType::Gc),
+    );
+    register_for_address(
+        "ChefKiss\\Extract SDMA firmware",
+        "",
+        ExtractorCommand::new(FirmwareType::Sdma),
     );
     true
 }
